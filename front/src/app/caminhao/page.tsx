c8623ef159dd5771620ac194b5b2c69a6537ba27
Joyuv2 <givanilson.fernandes@escolar.ifrn.edu.br>
2026-08-02 14:56:12 -0300

    página de lista e componentes

---
+++ front/src/app/caminhao/page.tsx
@@ -0,0 +1,64 @@
+// import '@/app/globals.css'
+
+export default function Page() {
+    const itens = [
+        {placa: "12345F", motorista:"juninho"},
+        {placa: "12345A", motorista:"juninho"},
+        {placa: "12345B", motorista:"juninho"},
+        {placa: "12345Z", motorista:"juninho"},
+        {placa: "12345V", motorista:"juninho"},
+        {placa: "12345F", motorista:"juninho"},
+        {placa: "12345A", motorista:"juninho"},
+        {placa: "12345B", motorista:"juninho"},
+        {placa: "12345Z", motorista:"juninho"},
+        {placa: "12345V", motorista:"juninho"},
+        {placa: "12345F", motorista:"juninho"},
+        {placa: "12345A", motorista:"juninho"},
+        {placa: "12345B", motorista:"juninho"},
+        {placa: "12345Z", motorista:"juninho"},
+        {placa: "12345V", motorista:"juninho"},
+        {placa: "12345F", motorista:"juninho"},
+        {placa: "12345A", motorista:"juninho"},
+        {placa: "12345B", motorista:"juninho"},
+        {placa: "12345Z", motorista:"juninho"},
+        {placa: "12345V", motorista:"juninho"},
+        {placa: "12345F", motorista:"juninho"},
+        {placa: "12345A", motorista:"juninho"},
+        {placa: "12345B", motorista:"juninho"},
+        {placa: "12345Z", motorista:"juninho"},
+        {placa: "12345V", motorista:"juninho"},
+        {placa: "12345F", motorista:"juninho"},
+        {placa: "12345A", motorista:"juninho"},
+        {placa: "12345B", motorista:"juninho"},
+        {placa: "12345Z", motorista:"juninho"},
+        {placa: "12345V", motorista:"juninho"},
+        {placa: "12345F", motorista:"juninho"},
+        {placa: "12345A", motorista:"juninho"},
+        {placa: "12345B", motorista:"juninho"},
+        {placa: "12345Z", motorista:"juninho"},
+        {placa: "12345V", motorista:"juninho"},
+        {placa: "12345F", motorista:"juninho"},
+        {placa: "12345A", motorista:"juninho"},
+        {placa: "12345B", motorista:"juninho"},
+        {placa: "12345Z", motorista:"juninho"},
+        {placa: "12345V", motorista:"juninho"}
+    ]
+    return(
+        <div className="p-12 w-full h-screen">
+            <div className="w-full h-full flex flex-row flex-wrap bg-background2 p-4 rounded-xl gap-5 overflow-scroll">
+                {itens.map((el, ind) => (
+                    <div key={ind} className="bg-background3 text-xl text-foreground h-55 w-45 rounded-xl p-2 flex flex-col justify-between">
+                        <div>
+                            <h1 className="font-bold">Placa: {el.placa}</h1>
+                            <h3>Motorista: {el.motorista}</h3>
+                        </div>
+                        <div className="flex flex-row justify-between opacity-70 font-bold">
+                            <div className="flex items-center justify-center rounded hover:cursor-pointer text-2xl bg-red-500 w-9 h-9 ">X</div>
+                            <div className="flex items-center justify-center rounded hover:cursor-pointer text-2xl bg-yellow-600 w-9 h-9 ">🖉</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
